adeb8df499ebe4d82893c3c05da12408719cebe7
alexlogsdon <77301656+alexlogsdon@users.noreply.github.com>
2021-04-29 18:43:14 -0400

    testing fixed line break on mobile. need to get font face to work next

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,8 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('src/img')
   eleventyConfig.addPassthroughCopy('admin')
+  eleventyConfig.addPassthroughCopy('src/styles/fonts')
+
 
   const {
     DateTime

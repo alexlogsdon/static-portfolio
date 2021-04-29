14bbc3d014d8eedf2ab6356e5dd4a42a21c41887
alexlogsdon <77301656+alexlogsdon@users.noreply.github.com>
2021-04-29 17:12:47 -0400

    Built global page structure, completed home page, added project page template

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'orange': '#f0460a',
+      }
+    },
   },
   variants: {},
   plugins: [],
